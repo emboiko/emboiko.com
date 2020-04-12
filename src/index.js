5c7035318b8fb95decfcf3df2f52995760bfac00
Edward Boiko <Emboiko@gmail.com>
2020-04-12 01:32:20 -0400

    about tweaks, dev/ is now tools/

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const flash = require("express-flash");
 const session = require("express-session");
 const methodOverride = require("method-override");
 const messageEmail = require("./email/email");
+const Post = require("./models/post");
 const { initializePassport, authenticated, notAuthenticated } = require("./auth/passport");
 require("./db/mongoose");
 
@@ -47,16 +48,11 @@ app.post("/contact", (req, res) => {
     let name = "Anonymous";
     let email = "Anonymous@emboiko.com";
 
-    if (req.body.email) {
-        email = req.body.email;
-    }
-    if (req.body.name) {
-        name = req.body.name;
-    }
+    if (req.body.email) email = req.body.email;
+    if (req.body.name) name = req.body.name;
 
     const message = req.body.compose;
     messageEmail(email, name, message);
-
     res.render("submitted", { message: "Message submitted. We will get back to you in less than 24 hours" });
 });
 
@@ -64,15 +60,36 @@ app.get("/signup", (req, res) => {
     res.render("signup");
 });
 
+app.get("/blog/:id", async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const post = await Post.findOne({ _id })
+        if (!post) return res.render("notfound");
+        res.render("blogpost", {
+            title: post.title,
+            body: post.body,
+            created: post.createdAt
+        });
+    } catch (err) {
+        res.render("notfound");
+    }
+});
 app.get("/blog", (req, res) => {
     res.render("blog");
-});
-app.get("/blog/:id", (req, res) => {
-    res.render("blogpost");
 });
 
 app.get("/compose", authenticated, (req, res) => {
     res.render("compose");
+});
+app.post("/compose", authenticated, async (req, res) => {
+    const post = new Post(req.body);
+    try {
+        await post.save((err, post) => {
+            res.redirect(`/blog/${post._id}`);
+        });
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 app.get("/login", notAuthenticated, (req, res) => {

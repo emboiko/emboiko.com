3cb0c15888e897b4b4bad86bfd7213d729c14e3e
Emboiko <emboiko@gmail.com>
2021-01-01 19:42:52 -0500

    use gmail account in email.js instead of roundcube's

--- src/email/email.js
+++ src/email/email.js
@@ -3,12 +3,12 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const messageEmail = (email, name, message) => {
-    sgMail.send({
-        to:"Ed@emboiko.com",
-        from:email,
-        subject:`Site Message from ${name}`,
-        text:message
-    });
+  sgMail.send({
+    to: "emboiko@gmail.com",
+    from: email,
+    subject: `Site Message from ${name}`,
+    text: message
+  });
 }
 
 module.exports = messageEmail;
